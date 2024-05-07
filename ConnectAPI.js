423de4790771efc349012a2b66813257339502fa
wilmarux <wilmar.cabezas@gmail.com>
2024-05-06 20:47:23 -0500

    first commit

--- ConnectAPI.js
+++ ConnectAPI.js
@@ -32,16 +32,6 @@ function ConnectAPI(props) {
         </View>
     )
 
-// Ejemplo de OpenAI para conectarnos.
-//   curl https://api.openai.com/v1/chat/completions \
-//   -H "Content-Type: application/json" \
-//   -H "Authorization: Bearer $OPENAI_API_KEY" \
-//   -d '{
-//      "model": "gpt-3.5-turbo",
-//      "messages": [{"role": "user", "content": "Say this is a test!"}],
-//      "temperature": 0.7
-//    }'
-//    Bearer Token: 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 
 }
 
 export default ConnectAPI;
